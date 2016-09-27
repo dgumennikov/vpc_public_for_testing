ec49824b1bd4f94c25448726a6d28847579ea210
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-09-27 15:38:19 -0700

    support for multiple regions

--- services/config.rb
+++ services/config.rb
@@ -9,10 +9,11 @@
 ######################################################################
 ## Create a vpc, igw and attach the igw to the vpc
 ######################################################################
-coreo_aws_vpc_vpc "${VPC_NAME}" do
+coreo_aws_vpc_vpc "${VPC_NAME}${SUFFIX}" do
   action :sustain
   cidr "${VPC_OCTETS}/16"
   internet_gateway true
+  region "${REGION}"
 end
 
 
@@ -20,13 +21,14 @@ end
 ## create a routetable for the public subnet, route everything
 ## to the internet gateway
 ######################################################################
-coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}" do
+coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}${SUFFIX}" do
   action :sustain
-  vpc "${VPC_NAME}"
+  vpc "${VPC_NAME}${SUFFIX}"
   routes [
-             { :from => "0.0.0.0/0", :to => "${VPC_NAME}", :type => :igw }
+             { :from => "0.0.0.0/0", :to => "${VPC_NAME}${SUFFIX}", :type => :igw }
         ]
   number_of_tables 1
+  region "${REGION}"
 end
 
 
@@ -36,11 +38,12 @@ end
 ## percent_of_vpc_allocated
 ##   split, but use only this percentage of the entire vpc range
 ######################################################################
-coreo_aws_vpc_subnet "${PUBLIC_SUBNET_NAME}" do
+coreo_aws_vpc_subnet "${PUBLIC_SUBNET_NAME}${SUFFIX}" do
   action :sustain
   number_of_zones 3
   percent_of_vpc_allocated 25
-  route_table "${PUBLIC_ROUTE_NAME}"
-  vpc "${VPC_NAME}"
+  route_table "${PUBLIC_ROUTE_NAME}${SUFFIX}"
+  vpc "${VPC_NAME}${SUFFIX}"
   map_public_ip_on_launch true
+  region "${REGION}"
 end
